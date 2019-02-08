a8408ce1da8e9652c4fa116379a7df58efcf08c3
Dallas Gale <dallasgale@hotmail.com>
2019-02-07 20:36:14 -0500

    contact form update

--- src/components/contact/contact.component.js
+++ src/components/contact/contact.component.js
@@ -8,45 +8,65 @@ import GitHubLogo_day from '../../images/logos/gh.png'
 // import PropTypes from 'prop-types';
 
 const Contact = props => {
-  const { mode } = props
+  const { mode, onFieldChange, onFormSubmit } = props
 
   return (
     <section className="section  contact">
       <div className="content  contact__wrapper">
         <div className="contact__content">
           <div className="contact__content-details">
-            <SectionTitle title="Get In Touch" color="positive" position="center" />
+            <SectionTitle
+              title="Get In Touch"
+              color="positive"
+              position="center"
+            />
             <h3>
               If you would like to discuss any future opportunities please
               contact me.
             </h3>
 
-
             {/* ADD TO A <Form /> component */}
             <div className="contact__form">
-              <form 
-                name="contact" 
-                method="POST" 
-                data-netlify="true" 
-                action="thanks/" 
-                netlify-honeypot="bot-field"
-                data-netlify-recaptcha="true">
-                  <div className="contact__form-fields">
-                    <h4 className="contact__form-fields--top  typography__h4  ">
-                      <label>Your Name <input type="text" name="name" /></label>   
-                    </h4>
-                    <h4 className="typography__h4  ">
-                      <label>Your Email <input type="email" name="email" /></label>
-                    </h4>
-                  </div>
-                    <h4 className="contact__form-fields--top">
-                      <label className="typography__h4  ">Message <textarea name="message"></textarea></label>
-                    </h4>
-                    <div data-netlify-recaptcha="true"></div>
-                    <h4 className="typography__h4">
-                      <button className="contact__button" type="submit">Send</button>
-                    </h4>
-                </form>
+              <form
+                name="contact"
+                method="post"
+                action="/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={onFormSubmit}
+              >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                <input type="hidden" name="form-name" value="contact" />
+                <p hidden>
+                  <label>
+                    Don’t fill this out:{' '}
+                    <input name="bot-field" onChange={onFieldChange} />
+                  </label>
+                </p>
+                <div className="contact__form-fields">
+                  <h4 className="contact__form-fields--top  typography__h4  ">
+                    <label>
+                      Your Name <input type="text" name="name" onChange={onFieldChange} />
+                    </label>
+                  </h4>
+                  <h4 className="typography__h4  ">
+                    <label>
+                      Your Email <input type="email" name="email" onChange={onFieldChange} />
+                    </label>
+                  </h4>
+                </div>
+                <h4 className="contact__form-fields--top">
+                  <label className="typography__h4  ">
+                    Message <textarea name="message" onChange={onFieldChange} />
+                  </label>
+                </h4>
+                <div data-netlify-recaptcha="true" />
+                <h4 className="typography__h4">
+                  <button className="contact__button" type="submit">
+                    Send
+                  </button>
+                </h4>
+              </form>
             </div>
 
             <ul className="contact__list">
@@ -55,7 +75,10 @@ const Contact = props => {
                   className="typography__weight--bold  typography__color--splash"
                   href="http://www.linkedin.com/in/dallas-gale"
                 >
-                  <img src={mode === 'night' ? (LinkedInLogo) : (LinkedInLogo_day)} alt="My LinkedIn profile" />
+                  <img
+                    src={mode === 'night' ? LinkedInLogo : LinkedInLogo_day}
+                    alt="My LinkedIn profile"
+                  />
                 </a>
               </li>
               <li className="contact__list-items--connect">
@@ -63,7 +86,10 @@ const Contact = props => {
                   className="typography__weight--bold  typography__color--splash"
                   href="http://github.com/dgale1983"
                 >
-                  <img src={mode === 'night' ? (GitHubLogo) : (GitHubLogo_day)} alt="My GitHub repository" />
+                  <img
+                    src={mode === 'night' ? GitHubLogo : GitHubLogo_day}
+                    alt="My GitHub repository"
+                  />
                 </a>
               </li>
             </ul>
--- src/containers/contact.container.js
+++ src/containers/contact.container.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import Contact from '../components/contact/contact.component';
+import { navigate } from 'gatsby';
 
+
+// Workaround for Netlify forms to play nice with Gatsby
+// https://github.com/imorente/gatsby-netlify-form-example/blob/master/src/pages/contact.js
+function encode(data) {
+    return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
 class ContactContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={};
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const form = e.target;
+        fetch("/", {
+            method: "POST",
+            headers: {"Content-Type": "application/x-www-form-urlencoded"},
+            body: encode({
+                "form-name": form.getAttribute("name"), ...this.state
+            })
+        })
+        .then(() => navigate(form.getAttribute("action")))
+        .catch(error => alert(error));
+    };
+    
     render() {
         return (
-            <Contact mode={this.props.mode} />
+            <Contact 
+                mode={this.props.mode}
+                onFieldChange={this.handleChange}
+                onFormSubmit={this.handleSubmit} />
         );
     }
 }
