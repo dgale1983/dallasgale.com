3de263076123b24c62d7859b358270c850eb7fe2
dgale1983 <dallasgale@hotmail.com>
2019-02-19 16:32:04 -0500

    updated builtwith component

--- src/components/builtwith/builtwith.component.js
+++ src/components/builtwith/builtwith.component.js
@@ -6,17 +6,19 @@ import './styles.scss';
 const BuiltWith = ({tech, theme}) => {
     return (
         <section className="section  builtwith">
-            <div className="builtwith__content">
-                <h5 className="typography__weight--thin  builtwith__title">Thanks to the below <span className="typography__weight--bold">
-                    <a className="builtwith__links" href="https://jamstack.org/">JAMSTACK</a> + Tech</span></h5>
-                <div className="builtwith__logos">
-                    {
-                        tech.map(t => (
-                            <a key={t.url} href={t.url}>
-                                <img key={t.name} src={`${theme === 'day' ? (t.logoDay) : (t.logo)}`} alt={`${t.tagline} - ${t.name}`} />
-                            </a>
-                        ))
-                    }
+            <div className="builtwith__wrapper">
+                <div className="builtwith__content">
+                    <h5 className="typography__weight--thin  builtwith__title">I choose to use the below <span className="typography__weight--bold">
+                        <a className="builtwith__links" href="https://jamstack.org/">stack</a></span></h5>
+                        <div className="builtwith__logos">
+                        {
+                            tech.map(t => (
+                                <a key={t.url} href={t.url}>
+                                    <img key={t.name} src={`${theme === 'day' ? (t.logoDay) : (t.logo)}`} alt={`${t.tagline} - ${t.name}`} />
+                                </a>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </section>
